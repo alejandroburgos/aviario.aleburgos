dea400e8f7072faed31dee9050bf03248fa949b4
Alejandro Burgos <aburgosm@eservicios.indra.es>
2022-08-02 10:40:59 +0200

    añadido option cors

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ const cors = require('cors')
 const path = require('path');
 
 app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header( "Access-Control-Allow-Headers", "Origen, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.options('*', cors()) // include before other routes
+
 const passport = require('passport')
 
 // cambio de puerto en heroku
