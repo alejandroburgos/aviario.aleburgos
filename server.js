67b48322068f10041c199f4d733c7d47a4763b55
Alejandro Burgos <aburgosm@eservicios.indra.es>
2022-08-02 09:48:02 +0200

    procfile a npm start

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const initDB = require('./config/db')
 const bodyParser = require('body-parser')
 const app = express()
 var cors = require('cors')
+
 const path = require('path');
 
 app.use(cors())
@@ -36,12 +37,10 @@ app.use(require('./src/routes/auth.js'))
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-if (process.env.NODE_ENV === "production") {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, "front/build")));
-    // Handle React routing, return all requests to React app
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "front/build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
 }
 
