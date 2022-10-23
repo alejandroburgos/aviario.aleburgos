458ff7fafa39cd955fa6340155341a72dd492eda
Alejandro Burgos <aburgosm@eservicios.indra.es>
2022-10-24 00:13:51 +0200

    add api/ to constant

--- front/src/Constants.js
+++ front/src/Constants.js
@@ -2,6 +2,6 @@ export const Colors = ["#1ec891", "#ff725e", "#ffd05b"];
 
 
 export const constants = {
-    urlLocal: "https://api.aviario.aleburgos.es/",
+    urlLocal: "https://api.aviario.aleburgos.es/api/",
     //urlLocal: "http://localhost:3001/api/",
 }
